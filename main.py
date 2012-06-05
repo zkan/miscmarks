c6cba9fec07edaee62b0e4da00c1334b5ed1ddec
Kan O <zkan.cs@gmail.com>
2012-06-05 10:26:58 +0700

    modify main and add a unit test file

--- main.py
+++ main.py
@@ -1,15 +1,203 @@
-import webapp2
-import template
+# code credit: CS253 WAE offered by Udacity
+
+import os
 import re
+import random
 import hashlib
 import hmac
-import json
-import time
 import logging
-from google.appengine.ext import db
-from google.appengine.api import memcache
+import json
+from string import letters
 
-# Global functions and variables
+import webapp2
+import jinja2
+
+from google.appengine.ext import db
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
+                               autoescape = True)
+
+secret = 'zkan'
+
+def render_str(template, **params):
+    t = jinja_env.get_template(template)
+    return t.render(params)
+
+def make_secure_val(val):
+    return '%s|%s' % (val, hmac.new(secret, val).hexdigest())
+
+def check_secure_val(secure_val):
+    val = secure_val.split('|')[0]
+    if secure_val == make_secure_val(val):
+        return val
+
+class MarkHandler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+
+    def render_str(self, template, **params):
+        params['user'] = self.user
+        t = jinja_env.get_template(template)
+        return t.render(params)
+
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
+    def render_json(self, d):
+        json_txt = json.dumps(d)
+        self.response.headers['Content-Type'] = 'application/json; charset=UTF-8'
+        self.write(json_txt)
+
+    def set_secure_cookie(self, name, val):
+        cookie_val = make_secure_val(val)
+        self.response.headers.add_header(
+            'Set-Cookie',
+            '%s=%s; Path=/' % (name, cookie_val))
+
+    def read_secure_cookie(self, name):
+        cookie_val = self.request.cookies.get(name)
+        return cookie_val and check_secure_val(cookie_val)
+
+    def login(self, user):
+        self.set_secure_cookie('user_id', str(user.key().id()))
+
+    def logout(self):
+        self.response.headers.add_header('Set-Cookie', 'user_id=; Path=/')
+
+    def initialize(self, *a, **kw):
+        webapp2.RequestHandler.initialize(self, *a, **kw)
+        uid = self.read_secure_cookie('user_id')
+        self.user = uid and User.by_id(int(uid))
+
+        if self.request.url.endswith('.json'):
+            self.format = 'json'
+        else:
+            self.format = 'html'
+
+class MainPage(MarkHandler):
+  def get(self):
+      self.write('Hello, Udacity!')
+
+
+##### user stuff
+
+def make_salt(length = 5):
+    return ''.join(random.choice(letters) for x in xrange(length))
+
+def make_pw_hash(name, pw, salt = None):
+    if not salt:
+        salt = make_salt()
+    h = hashlib.sha256(name + pw + salt).hexdigest()
+    return '%s,%s' % (salt, h)
+
+def valid_pw(name, password, h):
+    salt = h.split(',')[0]
+    return h == make_pw_hash(name, password, salt)
+
+def users_key(group = 'default'):
+    return db.Key.from_path('users', group)
+
+class User(db.Model):
+    name = db.StringProperty(required = True)
+    pw_hash = db.StringProperty(required = True)
+    email = db.StringProperty()
+
+    @classmethod
+    def by_id(cls, uid):
+        return User.get_by_id(uid, parent = users_key())
+
+    @classmethod
+    def by_name(cls, name):
+        u = User.all().filter('name =', name).get()
+        return u
+
+    @classmethod
+    def register(cls, name, pw, email = None):
+        pw_hash = make_pw_hash(name, pw)
+        return User(parent = users_key(),
+                    name = name,
+                    pw_hash = pw_hash,
+                    email = email)
+
+    @classmethod
+    def login(cls, name, pw):
+        u = cls.by_name(name)
+        if u and valid_pw(name, pw, u.pw_hash):
+            return u
+
+
+##### mark stuff
+
+def mark_key(name = 'default'):
+    return db.Key.from_path('marks', name)
+
+class Mark(db.Model):
+    title = db.StringProperty(required = True)
+    link = db.TextProperty(required = True)
+    desc = db.TextProperty(required = False)
+    created = db.DateTimeProperty(auto_now_add = True)
+    last_modified = db.DateTimeProperty(auto_now = True)
+
+    def render(self):
+        self._render_text = self.content.replace('\n', '<br>')
+        return render_str("mark.html", p = self)
+
+    def as_dict(self):
+        time_fmt = '%c'
+        d = {'title': self.title,
+             'link': self.link,
+             'desc': self.desc,
+             'created': self.created.strftime(time_fmt),
+             'last_modified': self.last_modified.strftime(time_fmt)}
+        return d
+
+
+
+class MarkFront(MarkHandler):
+    def get(self):
+        marks = greetings = Mark.all().order('-created')
+        if self.format == 'html':
+            self.render('front.html', marks = marks)
+        else:
+            return self.render_json([m.as_dict() for m in marks])
+
+class PostPage(MarkHandler):
+    def get(self, post_id):
+        key = db.Key.from_path('Post', int(post_id), parent=blog_key())
+        post = db.get(key)
+
+        if not post:
+            self.error(404)
+            return
+        if self.format == 'html':
+            self.render("permalink.html", post = post)
+        else:
+            self.render_json(post.as_dict())
+
+class NewPost(MarkHandler):
+    def get(self):
+        if self.user:
+            self.render("newpost.html")
+        else:
+            self.redirect("/login")
+
+    def post(self):
+        if not self.user:
+            self.redirect('/blog')
+
+        subject = self.request.get('subject')
+        content = self.request.get('content')
+
+        if subject and content:
+            p = Post(parent = blog_key(), subject = subject, content = content)
+            p.put()
+            self.redirect('/blog/%s' % str(p.key().id()))
+        else:
+            error = "subject and content, please!"
+            self.render("newpost.html", subject=subject, content=content, error=error)
+
+
 USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
 def valid_username(username):
     return username and USER_RE.match(username)
@@ -22,265 +210,103 @@ EMAIL_RE  = re.compile(r'^[\S]+@[\S]+\.[\S]+$')
 def valid_email(email):
     return not email or EMAIL_RE.match(email)
 
-COOKIE_RE = re.compile(r'.+=; Path=/')
-def valid_cookie(cookie):
-    return cookie and COOKIE_RE.match(cookie)
-
-def blog_key(name = 'default'):
-    return db.Key.from_path('blogs', name)
-
-# Data models
-class User(db.Model):
-    username = db.StringProperty(required = True)
-    password = db.TextProperty(required = True)
-    email = db.StringProperty(required = False)
-    created = db.DateTimeProperty(auto_now_add = True)
-    last_modified = db.DateTimeProperty(auto_now = True)
-
-class Post(db.Model):
-    subject = db.StringProperty(required = True)
-    content = db.TextProperty(required = True)
-    created = db.DateTimeProperty(auto_now_add = True)
-    last_modified = db.DateTimeProperty(auto_now = True)
-
-    def render(self):
-        self._render_text = self.content.replace('\n', '<br>')
-        return template.render_str("post.html", p = self)
-
-# Classes
-class BlogFront(template.TemplateHandler):
+class Signup(MarkHandler):
     def get(self):
-        age = memcache.get('age')
-        if age is None:
-            logging.error('AGE INITIALIZED')
-            age = 0
-            memcache.set('age', time.time())
-        else:
-            age = int(time.time() - age)
-            
-        logged_in = False
-        username = ''
-        user_id = self.request.cookies.get('user_id')
-        if user_id:
-            logged_in = True
-            key = db.Key.from_path('User', int(user_id), parent=blog_key())
-            u = db.get(key)
-            username = u.username
-            
-#        posts = db.GqlQuery("select * from Post order by created desc limit 10")      
-        posts = Post.all().order('-created')
-        self.render('front.html', logged_in = logged_in, 
-                                  username = username, 
-                                  posts = posts, 
-                                  age = age)
-
-class PostPage(template.TemplateHandler):
-    def get(self, post_id):
-        age = memcache.get('age%s' % post_id)
-        if age is None:
-            logging.error('AGE INITIALIZED')
-            age = 0
-            memcache.set('age%s' % post_id, time.time())
-        else:
-            age = int(time.time() - age)
-
-        key = db.Key.from_path('Post', int(post_id), parent=blog_key())
-        post = db.get(key)
-        
-        logged_in = False
-        username = ''
-        user_id = self.request.cookies.get('user_id')
-        if user_id:
-            logged_in = True
-            key = db.Key.from_path('User', int(user_id), parent=blog_key())
-            u = db.get(key)
-            username = u.username
-
-        if not post:
-            self.error(404)
-            return
-
-        self.render("permalink.html", logged_in = logged_in, 
-                                      username = username, 
-                                      post = post, 
-                                      age = age)
-
-class NewPost(template.TemplateHandler):
-    def get(self):
-        user_id = self.request.cookies.get('user_id')
-        if not user_id:
-           self.redirect('/blog/login')
-
-        self.render("newpost.html")
-
-    def post(self):
-        subject = self.request.get('subject')
-        content = self.request.get('content')
-
-        if subject and content:
-            p = Post(parent = blog_key(), 
-                     subject = subject, 
-                     content = content)
-            p.put()
-
-#            self.write(p.key().id())
-#            t = Post.get_by_id(p.key().id())
-#            self.write(t.subject)
-
-            self.redirect('/blog/%s' % str(p.key().id()))
-        else:
-            error = 'subject and content, please!'
-            self.render("newpost.html", subject = subject, 
-                                        content = content, 
-                                        error = error)
-
-class Welcome(template.TemplateHandler):
-    def get(self):
-        user_id = self.request.cookies.get('user_id')
-        if user_id:
-            key = db.Key.from_path('User', int(user_id), parent=blog_key())
-            u = db.get(key)
-            username = u.username
-
-            if valid_username(username):
-                self.render('welcome.html', username = username)
-            else: 
-                self.redirect('/blog/signup')
-        else: 
-            self.redirect('/blog/signup')
-
-class SignUp(template.TemplateHandler):
-    def get(self):
-        self.render('signup-form.html')
+        self.render("signup-form.html")
 
     def post(self):
         have_error = False
-        username = self.request.get('username')
-        password = self.request.get('password')
-        verify = self.request.get('verify')
-        email = self.request.get('email')
-        
-        params = dict(username = username, 
-                      email = email)
+        self.username = self.request.get('username')
+        self.password = self.request.get('password')
+        self.verify = self.request.get('verify')
+        self.email = self.request.get('email')
 
-        if not valid_username(username):
+        params = dict(username = self.username,
+                      email = self.email)
+
+        if not valid_username(self.username):
             params['error_username'] = "That's not a valid username."
             have_error = True
 
-        if not valid_password(password):
+        if not valid_password(self.password):
             params['error_password'] = "That wasn't a valid password."
             have_error = True
-        elif password != verify:
+        elif self.password != self.verify:
             params['error_verify'] = "Your passwords didn't match."
             have_error = True
 
-        if not valid_email(email):
+        if not valid_email(self.email):
             params['error_email'] = "That's not a valid email."
             have_error = True
-        
+
         if have_error:
             self.render('signup-form.html', **params)
         else:
-            query = db.GqlQuery("select * from User where username = :username", username = username)
-            user_in_db = query.fetch(1)
-            
-            if not user_in_db:
-                u = User(parent = blog_key(),
-                         username = username, 
-                         password = password, 
-                         email = email)
-                u.put()
+            self.done()
 
-                user_id = str(u.key().id())
-                cookie_str = str('user_id=%s; Path=/' % user_id)
-                self.response.headers['Content-Type'] = 'text/plain'
-                self.response.headers.add_header('Set-Cookie', cookie_str)
-                self.redirect('/blog/welcome')
-            else:
-               params['error_username'] = "That user already exists."
-               self.render('signup-form.html', **params)
+    def done(self, *a, **kw):
+        raise NotImplementedError
 
-class LogIn(template.TemplateHandler):
+#class Unit2Signup(Signup):
+#    def done(self):
+#        self.redirect('/unit2/welcome?username=' + self.username)
+
+class Register(Signup):
+    def done(self):
+        #make sure the user doesn't already exist
+        u = User.by_name(self.username)
+        if u:
+            msg = 'That user already exists.'
+            self.render('signup-form.html', error_username = msg)
+        else:
+            u = User.register(self.username, self.password, self.email)
+            u.put()
+
+            self.login(u)
+            self.redirect('/unit3/welcome')
+
+class Login(MarkHandler):
     def get(self):
         self.render('login-form.html')
-    
+
     def post(self):
-        have_error = False
-        username = str(self.request.get('username'))
-        password = str(self.request.get('password'))
-        
-        query = db.GqlQuery("select * from User where username = :username", username = username)
-        user_in_db = query.get()
-        if user_in_db:
-            if user_in_db.password == password:
-                user_id = user_in_db.key().id()
-                cookie_str = str('user_id=%s; Path=/' % user_id)
-                self.response.headers['Content-Type'] = 'text/plain'
-                self.response.headers.add_header('Set-Cookie', cookie_str)
-                self.redirect('/blog/welcome')
-            else:
-                have_error = True
+        username = self.request.get('username')
+        password = self.request.get('password')
+
+        u = User.login(username, password)
+        if u:
+            self.login(u)
+            self.redirect('/unit3/welcome')
         else:
-            have_error = True
-        
-        if have_error:
-            error = 'Invalid login'
-            self.render('login-form.html', username = username, error = error)
+            msg = 'Invalid login'
+            self.render('login-form.html', error = msg)
 
-class LogOut(template.TemplateHandler):
+class Logout(MarkHandler):
     def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        cookie_str = 'user_id=; Path=/'
-        if valid_cookie(cookie_str):
-            self.response.headers.add_header("Set-Cookie", cookie_str)
-            self.redirect('/blog/signup')
+        self.logout()
+        self.redirect('/signup')
 
-def generate_json(posts):
-    jc = ''
-#    xxx.strftime("%b %d, %Y")
-    if len(posts) > 1:
-        jc = ', '.join("{\"content\": \"%s\", \"created\": \"%s\", \"last_modified\": \"%s\", \"subject\": \"%s\"}" % (p.content.replace('"', '\\\\"').replace("'", '\\\''), p.created.strftime("%a %b %d %H:%M:%S %Y"), p.last_modified.strftime("%a %b %d %H:%M:%S %Y"), p.subject.replace('"', '\\\\"').replace("'", '\\\'')) for p in posts)
-        
-        jc = '[' + jc + ']'
-    else:
-        p = posts[0]
-        jc = "{\"content\": \"%s\", \"created\": \"%s\", \"last_modified\": \"%s\", \"subject\": \"%s\"}" % (p.content.replace('"', '\\\\"').replace("'", '\\\''), p.created.strftime("%a %b %d %H:%M:%S %Y"), p.last_modified.strftime("%a %b %d %H:%M:%S %Y"), p.subject.replace('"', '\\\\"').replace("'", '\\\''))
+#class Unit3Welcome(BlogHandler):
+#    def get(self):
+#        if self.user:
+#            self.render('welcome.html', username = self.user.name)
+#        else:
+#            self.redirect('/signup')
 
-    return jc
-
-class JSONPage(template.TemplateHandler):
-    def get(self, post_id):
-        self.response.headers['Content-Type'] = 'application/json; charset=UTF-8'
-        if post_id:
-            key = db.Key.from_path('Post', int(post_id), parent=blog_key())
-            post = db.get(key)
-
-            if not post:
-                self.error(404)
-                return
-
-            posts = [post]
-            self.write(generate_json(posts))
+class Welcome(MarkHandler):
+    def get(self):
+        username = self.request.get('username')
+        if valid_username(username):
+            self.render('welcome.html', username = username)
         else:
-            posts = Post.all().order('-created')
-            posts = list(posts)
-            self.write(generate_json(posts))
+            self.redirect('/unit2/signup')
 
-class FlushCache(template.TemplateHandler):
-    def get(self):
-        memcache.flush_all()
-        self.redirect('/blog')
-
-# Handlers
-app = webapp2.WSGIApplication([('/blog/?', BlogFront),
-                               ('/blog/(\d+)', PostPage),
-                               ('/blog/newpost', NewPost), 
-                               ('/blog/welcome', Welcome), 
-                               ('/blog/signup', SignUp), 
-                               ('/blog/login', LogIn),
-                               ('/blog/logout', LogOut), 
-                               ('/blog/flush', FlushCache), 
-                               ('/blog/(\d+)?\.json', JSONPage), 
-                               ], 
-                              debug = True)
-
+app = webapp2.WSGIApplication([('/', MainPage),
+                               ('/blog/?(?:.json)?', MarkFront),
+                               ('/blog/([0-9]+)(?:.json)?', PostPage),
+                               ('/blog/newpost', NewPost),
+                               ('/signup', Register),
+                               ('/login', Login),
+                               ('/logout', Logout),
+                               ],
+                              debug=True)
